0826e8edd27b4d3eeebb363c7f710aa76c6a18d2
이은지 <ohohdmswlgd@gmail.com>
2026-04-27 13:12:41 +0900

    홈화면 이미지, 상품리스트, 각 링크 설정

--- react-shop/src/App.jsx
+++ react-shop/src/App.jsx
@@ -2,45 +2,93 @@ import { useState } from 'react'
 import './App.css'
 import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import bongsdata from './data';
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
 
 function App() {
   let [bongs] = useState(bongsdata);
+
+  function BongCard(props) {
+    const navigate = useNavigate();
+    return (
+      <>
+        <Col sm={4} key={props.bongs.id} className='text-center' onClick={() => { navigate('/detail') }}>
+          <img
+            src={props.bongs.img}
+            style={{ width: '100%', maxWidth: '250px' }}
+          />
+          <h4>{props.bongs.title}</h4>
+          <p>{props.bongs.content}</p>
+          <p>{props.bongs.price}원💓</p>
+        </Col>
+      </>
+    )
+  }
+
+  function Home() {
+    return (
+      <>
+        <div className="main-bg"> </div>
+        <div className="container">
+          <Row>
+            {bongs.map(function (a, i) {
+              return <BongCard bongs={bongs[i]} i={i}></BongCard>
+            }
+            )}
+          </Row>
+        </div>
+      </>
+    )
+  }
+  function Detail(props) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={props.bongs.img} width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{props.bongs.title}</h4>
+            <p>{props.bongs.content}</p>
+            <p>{props.bongs.price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  function About() {
+    return (
+      <div>
+        <h4>about페이지임</h4>
+        <Outlet></Outlet>
+      </div>
+    )
+  }
   return (
     <div className="App">
+
       {/* 네비게이션 바 */}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"> </div>
-      <Container>
-        <Row>
-          <Col sm>
-            <div className='img1' />
-            <h4>{bongs[0].title}</h4>
-            <p>{bongs[0].content}</p>
-            <p>{bongs[0].price}원</p>
-          </Col>
-          <Col sm>
-            <div className='img2' />
-            <h4>{bongs[1].title}</h4>
-            <p>{bongs[1].content}</p>
-            <p>{bongs[1].price}원</p>
-          </Col>
-          <Col sm>
-            <div className='img3' />
-            <h4>{bongs[2].title}</h4>
-            <p>{bongs[2].content}</p>
-            <p>{bongs[2].price}원</p>
-          </Col>
-        </Row>
-      </Container>
+
+      <Link to="/">home</Link>
+      <Link to="/detail">상세페이지</Link>
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<div>없는페이지입니다</div>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
 
     </div>
   )
--- react-shop/src/data.js
+++ react-shop/src/data.js
@@ -1,23 +1,30 @@
+import img1 from "./assets/img1.png";
+import img2 from "./assets/img2.png";
+import img3 from "./assets/img3.png";
+
 let bongsdata = [
     {
         id: 0,
-        title: "Curious Bongbong",
-        content: "Bongbong the Curious:",
-        price: 12000000000
+        title: "봉봉이 프로필사진",
+        content: "봉봉이의 첫 번째 굿즈",
+        price: 120000,
+        img: img1
     },
 
     {
         id: 1,
-        title: "Balloons & Bongbong",
-        content: "Bongbong and Balloons",
-        price: 11000000000
+        title: "풍선과 봉봉이 액자",
+        content: "봉봉이와 풍선들",
+        price: 125000,
+        img: img2
     },
 
     {
         id: 2,
-        title: "Pac-Man & Bongbong",
-        content: "Bongbong and Pac-Man",
-        price: 13000000000
+        title: "팩맨과 봉봉이 액자",
+        content: "팩맨과 함께하는 봉봉이",
+        price: 128000,
+        img: img3
     }
 ]
 export default bongsdata
